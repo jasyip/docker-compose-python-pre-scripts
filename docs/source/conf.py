590075fa0122d9f551d352ce6fcb9b1e4dcba9a3
Jason Yip <general@jasonyip1.anonaddy.me>
2023-02-20 08:35:35 -0600

    [ADD]: `setup.cfg`, myst-parser docs dependency, dynamic VERSION

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,19 +6,25 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+from pathlib import Path
+
 project = 'Docker Compose Python Pre-Scripts'
 copyright = '2023, Jason Yip'
 author = 'Jason Yip'
-release = '0.1.0'
+release = (Path(__file__).parents[2] / "VERSION").read_text().strip()
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+        "sphinx.ext.autodoc",
+        ]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
+suppress_warnings = ["myst.header"]
+source_suffix = [".rst", ".md"]
 
 
 # -- Options for HTML output -------------------------------------------------
