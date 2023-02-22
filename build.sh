d96f1dc55cc2569a54d1e70d6d4c209f0fd38c0a
Jason Yip <general@jasonyip1.anonaddy.me>
2023-02-21 18:07:41 -0600

    [UPDATE]: docker build and run scripts support testing Python 3.9-11 together

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-podman build -t vps_python .
+: ${PYTHON_VERSIONS:="3.9 3.10 3.11"}
+
+for version in ${PYTHON_VERSIONS}; do
+    podman build --build-arg "PYTHON_VERSION=${version}" -t "vps_python:${version}" . || exit
+done
--- run.sh
+++ run.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
-podman run -t --rm \
-        --user podman \
-        --security-opt label=disable \
-        --device /dev/fuse \
-        vps_python
+: ${PYTHON_VERSIONS:="3.9 3.10 3.11"}
+
+for version in ${PYTHON_VERSIONS}; do
+    podman run -t --rm \
+            --user podman \
+            --security-opt label=disable \
+            --device /dev/fuse \
+            "vps_python:${version}"
+done
+
 
         # --entrypoint '/home/podman/test_podman.sh' \
