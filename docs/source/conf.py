14dc35a9b1c615137fdb2fecf691db525b3e2889
Jason Yip <general@jasonyip1.anonaddy.me>
2023-02-21 18:56:52 -0600

    [FORMAT]: `conf.py` using black

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,30 +17,30 @@ sys.path.insert(0, str(PurePath(__file__).parents[2]))
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Docker Compose Python Pre-Scripts'
-copyright = '2023, Jason Yip'
-author = 'Jason Yip'
+project = "Docker Compose Python Pre-Scripts"
+copyright = "2023, Jason Yip"
+author = "Jason Yip"
 release = (Path(__file__).parents[2] / "VERSION").read_text().strip()
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-        "sphinx.ext.autodoc",
-        "myst_parser",
-        ]
+    "sphinx.ext.autodoc",
+    "myst_parser",
+]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
 suppress_warnings = ["myst.header"]
 source_suffix = {
-    ".md" : "markdown",
+    ".md": "markdown",
 }
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
