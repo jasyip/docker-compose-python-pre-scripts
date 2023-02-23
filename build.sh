e4bc0bc51b03faf6a06bd8419d151305be37350d
Jason Yip <general@jasonyip1.anonaddy.me>
2023-02-22 19:36:07 -0600

    [UPDATE]: use `test.sh` instead of `build.sh` + `run.sh`

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-: ${PYTHON_VERSIONS:="3.9 3.10 3.11"}
-
-for version in ${PYTHON_VERSIONS}; do
-    podman build --build-arg "PYTHON_VERSION=${version}" -t "vps_python:${version}" . || exit
-done
--- test.sh
+++ test.sh
@@ -3,12 +3,10 @@
 : ${PYTHON_VERSIONS:="3.9 3.10 3.11"}
 
 for version in ${PYTHON_VERSIONS}; do
+    podman build --build-arg "PYTHON_VERSION=${version}" -t "vps_python:${version}" . || exit
     podman run -t --rm \
             --user podman \
             --security-opt label=disable \
             --device /dev/fuse \
             "vps_python:${version}" || exit
 done
-
-
-        # --entrypoint '/home/podman/test_podman.sh' \
