98d20cfa6fc144f1f9b0aa3fe3fa119f1e698f24
Jason Yip <general@jasonyip1.anonaddy.me>
2023-02-23 15:13:40 -0600

    [UPDATE]: Move `test.sh` into `tests` folder

--- tests/test.sh
+++ tests/test.sh
@@ -2,6 +2,8 @@
 
 : ${PYTHON_VERSIONS:="3.9 3.10 3.11"}
 
+cd ..
+
 for version in ${PYTHON_VERSIONS}; do
     podman build --build-arg "PYTHON_VERSION=${version}" -t "vps_python:${version}" . || exit
     podman run -t --rm \
