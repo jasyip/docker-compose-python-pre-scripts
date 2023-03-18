e0fb04d67a2c3726891baaaff73468e35648338d
Jason Yip <general@jasonyip1.anonaddy.me>
2023-03-18 14:47:44 -0700

    [ADD]: Parametrization of `test_get_vol_dirs`

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,13 +1,14 @@
-import sys
+from itertools import chain, combinations
 from pathlib import PurePath
 
 import pytest
 
-sys.path.append(str(PurePath(__file__).parents[1]))
+import utils
 
-from functions import _VolDir, copy_to_volume
 
-"""
-def test_get_vol_dirs():
+_combination_lens = frozenset(range(1, len(utils.DATA_DIRS) + 1)) - frozenset(range(3, len(utils.DATA_DIRS) - 1))
+
+@pytest.mark.parametrize("data_path_powerset", map(frozenset, chain.from_iterable(combinations(utils.DATA_DIRS, r) for r in _combination_lens)))
+def test_get_vol_dirs(data_path_powerset):
     pass
-"""
+    # print(data_path_powerset)
